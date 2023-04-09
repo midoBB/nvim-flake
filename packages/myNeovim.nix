94f3e1cdb59d4e86fb7dd11a074f849ec5b930fc
Mohamed Hamdi <haamdi@outlook.com>
2023-04-09 14:13:15 +0100

    sksk

--- packages/myNeovim.nix
+++ packages/myNeovim.nix
@@ -1,8 +1,19 @@
-{ pkgs }:
-let
-  customRC = import ../config { inherit pkgs; };
-  plugins = import ../pluginsList.nix { inherit pkgs; };
-  runtimeDeps = import ../runtimeDeps.nix { inherit pkgs; };
+{pkgs}: let
+  recursiveMerge = attrList: let
+    f = attrPath:
+      builtins.zipAttrsWith (n: values:
+        if pkgs.lib.tail values == []
+        then pkgs.lib.head values
+        else if pkgs.lib.all pkgs.lib.isList values
+        then pkgs.lib.unique (pkgs.lib.concatLists values)
+        else if pkgs.lib.all pkgs.lib.isAttrs values
+        then f (attrPath ++ [n]) values
+        else pkgs.lib.last values);
+  in
+    f [] attrList;
+  customRC = import ../config {inherit pkgs;};
+  plugins = import ../pluginsList.nix {inherit pkgs;};
+  runtimeDeps = import ../runtimeDeps.nix {inherit pkgs;};
   neovimRuntimeDependencies = pkgs.symlinkJoin {
     name = "neovimRuntimeDependencies";
     paths = runtimeDeps.deps1;
@@ -11,16 +22,22 @@ let
     name = "neovimRuntimeDependencies2";
     paths = runtimeDeps.deps2;
   };
-  myNeovimUnwrapped = pkgs.wrapNeovim pkgs.neovim {
+
+  neovimaugmented = recursiveMerge [
+    pkgs.neovim-unwrapped
+    {buildInputs = neovimRuntimeDependencies;}
+  ];
+  myNeovimUnwrapped = pkgs.wrapNeovim neovimaugmented {
     configure = {
       inherit customRC;
       packages.all.start = plugins;
     };
   };
-in pkgs.writeShellApplication {
-  name = "nvim";
-  runtimeInputs = [ neovimRuntimeDependencies2 neovimRuntimeDependencies ];
-  text = ''
-    ${myNeovimUnwrapped}/bin/nvim "$@"
-  '';
-}
+in
+  pkgs.writeShellApplication {
+    name = "nvim";
+    runtimeInputs = [neovimRuntimeDependencies2 neovimRuntimeDependencies];
+    text = ''
+      ${myNeovimUnwrapped}/bin/nvim "$@"
+    '';
+  }
